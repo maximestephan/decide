920aa7fe8bc5b3b94ab33034141492296f1baa81
Stephan Maxime CHST <u974354@syngenta.org>
2024-01-18 13:33:35 +0100

    wikipedia working example

--- helpers.py
+++ helpers.py
@@ -1,10 +1,13 @@
 import io
 import base64
 import cmath
-#from shapely.geometry import Polygon
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
 from rdkit import Chem
+from rdkit.Chem import Descriptors
+
 
 def calculate_pBP8x(list_tupple_rate_efficacy, x):
     pBP8x = 0
@@ -37,23 +40,6 @@ def Area(corners):
     area = abs(area) / 2.0
     return area
 
-
-def calculate_biological_area2(list_tupple_rate_efficacy):
-    copy_list = list_tupple_rate_efficacy
-    origin_list = []
-    for index, tupple_rate_efficay in enumerate(copy_list):
-        copy_list[index] = (  tupple_rate_efficay[0] /10 , tupple_rate_efficay[1])
-        origin_list.append( (tupple_rate_efficay[0] /10 , 0))
-    origin_list.reverse()
-    t = tuple(copy_list + origin_list)
-    polygon = Polygon(t)
-    plt.plot(*polygon.exterior.xy)
-    plt.show()
-    return round( polygon.area / 100  ,2 )
-
-def transform(x):
-    #return x
-    return cmath.log(x).real
 
 def display_biological_area(list_tupple_rate_efficacy):
     n = len(list_tupple_rate_efficacy) 
@@ -95,6 +81,7 @@ def display_biological_area(list_tupple_rate_efficacy):
     plt.savefig(my_stringIObytes, format='png' )
     my_stringIObytes.seek(0)
     img_as_base64 = base64.b64encode(my_stringIObytes.read()).decode()
+    plt.close()
 
     return img_as_base64
     
@@ -111,74 +98,37 @@ def calculate_biological_area(list_tupple_rate_efficacy):
         area += area_rectangle + area_triangle
     return round(area / 100 ,2 ) 
     
+def using_display_biological_area():
+    results = None #getResults()
+    html = ""
+    for result in results:
+    
+        base64img = display_biological_area( result[1] )
+        html += f"<img  src='data:image/jpeg;base64,{base64img}' >"
+    return html
+
 
 def cleanFile(filename):
-    
     df = pd.read_csv(filename)
 
     #PandasTools.AddMoleculeColumnToFrame(df, 'SMILES', 'Molecule')
-    df['Molecule'] = df['SMILES'].apply(lambda x: Chem.MolFromSmiles(x, sanitize=False) )
-
+    df['Molecule'] = df['SMILES'].apply(lambda x: Chem.MolFromSmiles(x, sanitize=True) )
+    
     # if Molecule is None then Molecule become empty molecule
     df['Molecule'] = df['Molecule'].apply(lambda Molecule: Molecule if Molecule else Chem.MolFromSmiles('') )
-    df['InchiKey'] = df['Molecule'].apply(lambda x: Chem.InchiToInchiKey(Chem.MolToInchi(x, options='-KET -15T')))
-    df = df[['InchiKey','Name','SMILES']]
+    #df['InchiKey'] = df['Molecule'].apply(lambda x: Chem.InchiToInchiKey(Chem.MolToInchi(x, options='-KET -15T')))
+    #df = df[['InchiKey','Name','SMILES']]
 
-    #df.drop(columns=['Molecule'], inplace=True)
-    #filtered_df = df[df['InchiKey'].isnull()]
 
-    df.to_csv(filename + '_out.csv')
+    df['logP'] = df['Molecule'].apply(lambda mol: round(Descriptors.MolLogP(mol),1) )
+
+    descrs = [Descriptors.CalcMolDescriptors(mol) for mol in df['Molecule'].values.tolist()]
+    df = df.join(pd.DataFrame(descrs))
+     
+    df.drop(columns=['Molecule'], inplace=True)
+    df.drop(columns=df.columns[0], axis=1, inplace=True)
+    df.to_csv(filename + '_out.csv',index=False)
             
 
-    #print(filtered_df)
 
-#list_tupple_rate_efficacy = [(100, 90),(50, 20),(25, 20),(13, 0),(6, 0),(3, 0)]
-#score = calculate_biological_score(list_tupple_rate_efficacy)
-#area = calculate_biological_area(list_tupple_rate_efficacy)
-#print(score,area)
-
-#list_tupple_rate_efficacy = [(100, 100),(50, 100),(25, 100),(13, 100),(6, 100),(3, 100)]
-#score = calculate_biological_score(list_tupple_rate_efficacy)
-#area = calculate_biological_area(list_tupple_rate_efficacy)
-#print(score,area)
-
-
-results = [ 
-(1.26,[(100,96),(50,96),(25,83)] ) , 
-(1.05,[(100,73)]),
-(0.78,[(100,77),(50,70),(25,62)]),
-(1.50,[(100,100),(50,100),(25,88)]),
-(1.44,[(100,100),(50,97),(25,84)]),
-(1.21,[(100,97),(50,88),(25,84)]),
-(1.43,[(100,96),(50,54),(25,61),(13,72),(6,79)]),
-(3.50,[(100,100),(50,100),(25,100),(13,100),(6,100),(3,100)]),
-(3.50,[(100,100),(50,100),(25,100),(13,100),(6,100),(3,100)]),
-(3.50,[(100,100),(50,100),(25,100),(13,100),(6,100),(3,100)]),
-(1.67,[(100,90),(50,20),(25,20),(13,0),(6,0),(3,0)]),
-(0.00,[(100,0),(50,0),(25,0),(13,0),(6,0),(3,0)]),
-(3.50,[(100,100),(50,100),(25,100),(13,100),(6,100),(3,100)]),
-(3.50,[(100,100),(50,100),(25,100),(13,100),(6,100),(3,100)]),
-(2.47,[(100,100),(50,100),(25,70),(13,20),(6,0),(3,0)]),
-(3.45,[(100,100),(50,100),(25,100),(13,100),(6,100),(3,90)]),
-(2.75,[(100,100),(50,100),(25,100),(13,70),(6,0),(3,0)]),
-(1.40,[(100,100),(50,95),(25,60),(13,35),(6,10),(3,0)]),
-(1.02,[(100,90),(50,70),(25,35),(13,10),(6,10),(3,10)]),
-(0.13,[(100,35),(50,20),(25,0),(13,0),(6,0),(3,0)]),
-(0.09,[(100,25),(50,25),(25,25),(13,10),(6,0),(3,0)]),
-(1.07,[(100,100),(50,45),(25,10),(13,10),(6,0),(3,0)]),
-(0.31,[(100,50),(50,20),(25,20),(13,0),(6,0),(3,0)]),
-(1.04,[(100,90),(50,70),(25,45),(13,10),(6,0),(3,10)]),
-(0.40,[(100,60),(50,20),(25,0),(13,0),(6,0),(3,0)]),
-(1.87,[(100,100),(50,100),(25,90),(13,80),(6,60),(3,20)]),
-(0.91,[(100,70),(50,70),(25,70),(13,0),(6,35),(3,35)])
-]
-
-
-results2 = [
-#(5,[(50,100),(12.5,100),(3.125,100),(0.7813,100),(0.1953,100),(0.0488,100)]),
-(3.5,[(50,100),(12.5,70),(3.125,100),(0.7813,100),(0.1953,100),(0.0488,70)]),
-#(3,[(50,100),(12.5,100),(3.125,100),(0.7813,100),(0.1953,100),(0.0488,0)]),
-
-]
-
-plt.xlabel('Concentration')
+#cleanFile('data\wikipedia.csv')
--- main.py
+++ main.py
@@ -1,34 +1,87 @@
 import pandas as pd
 import requests, os
-from flask import request
+from flask import request, Response
 from time import sleep
 from rdkit import Chem
 from rdkit.Chem import PandasTools
-from rdkit.Chem import rdFMCS 
-from rdkit import DataStructs
-from rdkit import RDConfig
+
 from rdkit.Chem.Draw import rdMolDraw2D
-from rdkit.Chem import rdRGroupDecomposition as rdRGD
-from rdkit.Chem import rdmolops
+
 from rdkit.Chem import Draw
 from json import dumps
 from flask import Flask, render_template
 import base64
+import uuid
 import hashlib
 import helpers
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
+filename = 'data\wikipedia.csv'
 
 @app.route('/')
 def search():
-    data = [(50,100),(12.5,90),(3.125,100),(0.7813,70),(0.1953,50),(0.0488,20)]
+    uid = uuid.uuid4().hex
+    if filename.endswith('.csv'):
+        print('Loading csv')
+        df = pd.read_csv(filename, nrows= 999999)
+        #PandasTools.AddMoleculeColumnToFrame(df, 'SMILES', 'Molecule')
+        df['Molecule'] = df['SMILES'].apply(lambda x: Chem.MolFromSmiles(x, sanitize=True) )
+        # if Molecule is None then Molecule become empty molecule
+        df['Molecule'] = df['Molecule'].apply(lambda Molecule: Molecule if Molecule else Chem.MolFromSmiles('') )
+    elif filename.endswith('.sdf'):
+        print('Loading sdf')
+        #PandasTools.WriteSDF(df, 'data\wikipedia.sdf', molColName='Molecule', idName=None, properties=list(df.columns) )
+        df = PandasTools.LoadSDF(filename, molColName='Molecule')
+        #mols = Chem.MultithreadedSDMolSupplier(filename, numWriterThreads=8, molCol='Molecule')
 
-    base64img = helpers.display_biological_area( data )
-    html = f"<img  src='data:image/jpeg;base64,{base64img}' >"
-    return html
-    return render_template('main.html')
-    "*/width='100' height='100'/*"
 
 
+    df.insert(0, 'Molecule', df.pop('Molecule'))
+    globals()[uid] = df 
+
+    table = "<table class='maintable'>"
+    for index, row in df.iterrows():
+        if (index == 0 ):
+            table += "<tr>"
+            for column in df.columns:
+                column_name = str(column).replace('_',' ').replace('*',' ').replace('.','<br>')
+                table += "<th name='"+ str(column) + "' onclick='sortHeader(this)'  title='Click to sort by this column'>" + column_name + "</th>"        
+            table += "</tr>"    
+        table += f"<tr id='{str(index)}' class='observable' ></tr>"
+    table += "</table>"
+    return render_template('main.html', table=table, uid = uid)
+    
+
+
+@app.route("/readrow/" , methods = ['POST'])
+def readrow():
+    session = str(request.form.get('session'))  
+    index = int(str(request.form.get('index')))
+    try:
+        df = globals()[session]
+        row = df.iloc[index] #.query('index == ' + str(index) )
+        return renderRow(row, df.columns)
+    except:
+        return Response("", status=204)
+    
+
+
+def renderRow(row, columns):
+    table = "<tr>"
+    for column in columns:
+        if ( column == "Molecule"):
+            try:
+                drawer = rdMolDraw2D.MolDraw2DCairo(200, 100)
+                drawer.drawOptions().useBWAtomPalette()
+                drawer.SetLineWidth(0.9)
+                drawer.drawOptions().minFontSize = 9
+                rdMolDraw2D.PrepareAndDrawMolecule(drawer, row['Molecule'])                
+                table += f"""<td class='stickyImage'><div style=' height: 100px; width: 200px;'><img src='data:image/png;base64, {base64.b64encode(drawer.GetDrawingText()).decode('utf8')}' /></div></td>"""
+            except Exception as ex :
+                print("An exception occurred ")
+                table += f"<td class='stickyImage'><div style=' display: table-cell;  border: 0px solid black; height: 104px; width: 204px;'><img src='data:image/png;base64,xxx' /></div></td>"
+        else:
+                table += "<td>" + str(row[column]) + "</td>"
+    table += "</tr>"
+    return table
